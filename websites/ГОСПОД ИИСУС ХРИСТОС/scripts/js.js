020c1bd3bb097c6ee93f4154910a04d070ac6ad6
Georgi <friendship_forever@abv.bg>
2015-12-06 23:38:21 +0200

    directory of websites was altered

--- websites/ГОСПОД ИИСУС ХРИСТОС/scripts/js.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$('article').mouseover(function() {
-		$(this).stop().animate({'opacity': '0.7'}, 400);
-	});
-	$('article').mouseout(function() {
-		$(this).stop().animate({'opacity': '1'}, 400);
-	});
-});
