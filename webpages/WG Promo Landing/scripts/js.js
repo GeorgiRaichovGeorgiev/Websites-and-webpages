d7e0f7ccd3f30a61687c252448a9c915019c5742
Georgi <friendship_forever@abv.bg>
2015-12-06 23:35:35 +0200

    webpages were deleted

--- webpages/WG Promo Landing/scripts/js.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$('.social-link').mouseover(function() {
-		$(this).next().addClass('message-active');
-	});
-	$('.social-link').mouseout(function() {
-		$(this).next().removeClass('message-active');
-	});
-});
